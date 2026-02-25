b0433aff71b1eec0084787afe734d683d97373e1
Hec-Mejorada <hectormejorada95@gmail.com>
2026-02-24 22:16:46 -0600

    Update ej3.c

--- ejercicios/ej3.c
+++ ejercicios/ej3.c
@@ -1,6 +1,23 @@
 
 #include <stdio.h>
 int main(){
-    // Ejercicio 3
+
+    int a[3];
+    int b[3];
+    int c[3];
+    int i,j,k;
+    for ( i=0; i < 3; i++)
+    {  
+    printf("&a[%d]=%p\n" ,i,&a[i]);
+    }
+    for ( j=0; j < 3; j++)
+    {  
+    printf("&b[%d]=%p\n" ,j,&b[j]);
+    }
+    for ( k=0; k < 3; k++)
+    {  
+    printf("&c[%d]=%p\n" ,k,&c[k]);
+    } 
+
     return 0;
 }
